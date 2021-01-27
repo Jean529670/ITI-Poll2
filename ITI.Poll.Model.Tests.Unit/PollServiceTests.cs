42cf68f6a6cacb1abc9507833bfb30ce82317a69
Max <clairson.max@gmail.com>
2021-01-27 18:45:15 +0100

    Test.Unit => create_poll from PollService

--- ITI.Poll.Model.Tests.Unit/PollServiceTests.cs
+++ ITI.Poll.Model.Tests.Unit/PollServiceTests.cs
@@ -11,27 +11,27 @@ namespace ITI.Poll.Model.Tests.Unit
         [Test]
         public async Task create_poll()
         {
-            //IUnitOfWork unitOfWork = Substitute.For<IUnitOfWork>();
-            //unitOfWork.
+            IUnitOfWork unitOfWork = Substitute.For<IUnitOfWork>();
+            
+            IPollRepository pollRepository = Substitute.For<IPollRepository>();
+            pollRepository.Create(Arg.Any<Poll>()).Returns(Task.FromResult(Result.CreateSuccess()));
 
-            //TestHelpers testHelpers = new 
+            IUserRepository userRepository = Substitute.For<IUserRepository>();
+            userRepository.Create(Arg.Any<User>()).Returns(Task.FromResult(Result.CreateSuccess()));
+            User user = new User(1234, "toto@mail.com", "toto", "passwordhash", false);
+            userRepository.FindById(Arg.Any<int>()).Returns(Task.FromResult(Result.CreateSuccess(user)));
+            userRepository.FindByNickname(Arg.Any<string>()).Returns(Task.FromResult(Result.CreateSuccess(user)));
 
-            //IPollRepository pollRepository = Substitute.For<IPollRepository>();
-            //pollRepository.Create(Arg.Any<Poll>()).Returns(Task.FromResult(Result.CreateSuccess()));
+            NewPollDto newPollDto = new NewPollDto();
+            newPollDto.AuthorId = 1234;
+            newPollDto.Question = "Question?";
+            newPollDto.GuestNicknames = new string[] { "Guest1" };
+            newPollDto.Proposals = new string[] { "P1", "P2" };
+            PollService sut = new PollService();
 
-            //IUserRepository userRepository = Substitute.For<IUserRepository>();
-            //userRepository.Create(Arg.Any<User>()).Returns(Task.FromResult(Result.CreateSuccess()));
+            Result<Poll> poll = await sut.CreatePoll(unitOfWork, pollRepository, userRepository, newPollDto);
 
-            ////NewPollDto newPollDto = Substitute.For<NewPollDto>();
-            //NewPollDto newPollDto = new NewPollDto();
-            //newPollDto.AuthorId =0;
-            //newPollDto.Question = "Question?";
-            //PollService sut = new PollService();
-
-            //Result<Poll> poll = await sut.CreatePoll(unitOfWork, pollRepository, userRepository, newPollDto);
-
-            //Result<Poll> expected = Result.CreateSuccess(new Poll(0, 0, "Question?", false));
-            //poll.Should().BeEquivalentTo(expected);
+            poll.IsSuccess.Should().BeTrue();
         }
     }
 }
