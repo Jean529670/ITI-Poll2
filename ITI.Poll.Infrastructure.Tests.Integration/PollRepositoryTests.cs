1afe00fa3057ed2ecebcc925c713edf4f00c5b2c
Max <clairson.max@gmail.com>
2021-01-27 17:43:48 +0100

    Test.Integration => create_poll from pollRepository and fix pollService

--- ITI.Poll.Infrastructure.Tests.Integration/PollRepositoryTests.cs
+++ ITI.Poll.Infrastructure.Tests.Integration/PollRepositoryTests.cs
@@ -1,4 +1,9 @@
+using FluentAssertions;
+using ITI.Poll.Model;
+using ITI.Poll.Tests;
 using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
 
 namespace ITI.Poll.Infrastructure.Tests.Integration
 {
@@ -6,9 +11,33 @@ namespace ITI.Poll.Infrastructure.Tests.Integration
     public class PollRepositoryTests
     {
         [Test]
-        public void ____()
+        public async Task create_poll()
         {
-            
+            using (PollContext pollContext = TestHelpers.CreatePollContext())
+            {
+                PollContextAccessor pollContextAccessor = new PollContextAccessor(pollContext);
+                PollRepository sut = new PollRepository(pollContextAccessor);
+
+                // We should create user for create poll associated with this user
+                UserRepository userRepository = new UserRepository(pollContextAccessor);
+                string email = $"{Guid.NewGuid()}@test.org";
+                string nickname = $"Test-{Guid.NewGuid()}";
+                User user = new User(0, email, nickname, "hash", false);
+                Result userCreated = await userRepository.Create(user);
+
+                Model.Poll poll = new Model.Poll(0, user.UserId, "Question?", false);
+                
+                Result creationStatus = await sut.Create(poll);
+
+                userCreated.IsSuccess.Should().BeTrue();
+                creationStatus.IsSuccess.Should().BeTrue();
+                Result<Model.Poll> foundPoll = await sut.FindById(poll.PollId);
+                foundPoll.IsSuccess.Should().BeTrue();
+                foundPoll.Value.AuthorId.Should().Be(poll.AuthorId);
+                foundPoll.Value.PollId.Should().Be(poll.PollId);
+                foundPoll.Value.Question.Should().Be(poll.Question);
+                foundPoll.Value.IsDeleted.Should().BeFalse();
+            }
         }
     }
 }
--- ITI.Poll.Model.Tests.Integration/PollServiceTests.cs
+++ ITI.Poll.Model.Tests.Integration/PollServiceTests.cs
@@ -2,9 +2,10 @@ using FluentAssertions;
 using ITI.Poll.Infrastructure;
 using ITI.Poll.Tests;
 using NUnit.Framework;
+using System;
 using System.Threading.Tasks;
 
-namespace ITI.Poll.Model.Tests.Unit
+namespace ITI.Poll.Model.Tests.Integration
 {
     [TestFixture]
     public class PollServiceTests
@@ -16,17 +17,32 @@ namespace ITI.Poll.Model.Tests.Unit
             {
                 PollContextAccessor pollContextAccessor = new PollContextAccessor(pollContext);
                 PollRepository pollRepository = new PollRepository(pollContextAccessor);
+
+                // We should create Author user for create poll associated with this user
                 UserRepository userRepository = new UserRepository(pollContextAccessor);
+                string emailAuthor = $"{Guid.NewGuid()}@test.org";
+                string nicknameAuthor = $"Test-{Guid.NewGuid()}";
+                User userAuthor = new User(0, emailAuthor, nicknameAuthor, "hash", false);
+                Result userAuthorCreated = await userRepository.Create(userAuthor);
+
+                // We should create Author user for create poll associated with this user
+                string emailGuest = $"{Guid.NewGuid()}@test.org";
+                string nicknameGuest = $"Test-{Guid.NewGuid()}";
+                User userGuest = new User(0, emailGuest, nicknameGuest, "hash", false);
+                Result userGuestCreated = await userRepository.Create(userGuest);
 
                 NewPollDto newPollDto = new NewPollDto();
-                newPollDto.AuthorId = 0;
+                newPollDto.AuthorId = userAuthor.UserId;
                 newPollDto.Question = "Question?";
-                PollService sut = new PollService();
+                newPollDto.GuestNicknames = new string[]{ userGuest.Nickname };
+                newPollDto.Proposals = new string[] { "P1", "P2" };
 
-                Result<Poll> poll = await sut.CreatePoll(pollContext, pollRepository, userRepository, newPollDto);
+                Result<Poll> poll = await TestHelpers.PollService.CreatePoll(pollContext, pollRepository, userRepository, newPollDto);
 
-                Result<Poll> expected = Result.CreateSuccess(new Poll(0, 0, "Question?", false));
-                poll.Should().BeEquivalentTo(expected);
+
+                poll.IsSuccess.Should().BeTrue();
+                Result<Poll> foundPoll = await TestHelpers.PollService.FindById(pollRepository, poll.Value.PollId);
+                poll.Should().BeEquivalentTo(foundPoll);
             }
         }
     }
